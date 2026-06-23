8967f91d233eaebb4603b157ddfcd50be9f8bca4
Edgar A. Barragán <edgar@MacBook-Air-de-Edgar.local>
2026-06-23 11:05:50 -0500

    fix: Adjust image object-position to top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
               src={cataImg} 
               alt="Cata Ayala" 
               fill 
-              className="object-cover"
+              className="object-cover object-top"
               priority
             />
           </div>
